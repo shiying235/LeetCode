c545b2c5e4cc472bfeca96c9fd6a6f0cd8b50adb
Shiying Li <emma.lishiying@gmail.com>
2016-05-19 19:55:16 -0400

    [283. Move Zeroes][Compile Error]committed by LeetCode Extension

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -1,12 +1,21 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        for(int i=0,j=0;i<nums.length;i++){
-            if(nums[j]==0&&nums[i]!=0){
-                nums[j]=nums[i];
-                nums[i]=0;
-                j++;
-            }else if(nums[j]!=0) j++; 
+        // for(int i=0,j=0;i<nums.length;i++){
+        //     if(nums[j]==0&&nums[i]!=0){
+        //         nums[j]=nums[i];
+        //         nums[i]=0;
+        //         j++;
+        //     }else if(nums[j]!=0) j++; 
             
+        // }
+        if(nums.length<2) return null;
+        int count=0;
+        for(int i=0;i<nums.length;++i){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
         }
+        for(;count<nums.length;count++)
+            nums[count]=0;
     }
 }
