e76935a63009a4c4a9374cee80a09e2aaa27caad
Shiying Li <emma.lishiying@gmail.com>
2016-05-19 15:11:52 -0400

    [169. Majority Element][Compile Error]committed by LeetCode Extension

---
+++ 169-Majority-Element/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[n/2];
+        //return findMajority(0,nums.length-1,nums);
+    }
+    
+    // public int findMajority(int start,int end,int[] nums){
+    //     int length=end-start;
+    //     if(length<2&&nums!=null) return nums[start];
+    //     int sub1=findMajority(start,start+length/2,nums);
+    //     int sub2=findMajority(start+length/2+1,end,nums);
+    //     if(sub1==sub2) return sub1;
+        
+    // }
+}
