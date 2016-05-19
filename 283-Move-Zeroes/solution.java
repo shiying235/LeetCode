52f359c9d15ac718ef41a809f1ebfefff36b3576
Shiying Li <emma.lishiying@gmail.com>
2016-05-19 19:57:58 -0400

    [283. Move Zeroes][Accepted]committed by LeetCode Extension

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -1,24 +1,13 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        // if(nums.length<2) return;
-        // int count=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]!=0){
-        //         nums[count++]=nums[i];
-        //     }
-        // }
-        // for(;count<nums.length;count++)
-        //     nums[count]=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[count++]=nums[i];
+            }
+        }
+        for(;count<nums.length;count++)
+            nums[count]=0;
         
-        if (nums == null || nums.length == 0) return;        
-
-    int insertPos = 0;
-    for (int num: nums) {
-        if (num != 0) nums[insertPos++] = num;
-    }        
-
-    while (insertPos < nums.length) {
-        nums[insertPos++] = 0;
-    }
     }
 }
