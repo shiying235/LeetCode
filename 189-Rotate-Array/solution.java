452c482c6b139e2788fce6386b6e4cd23b573381
Shiying Li <emma.lishiying@gmail.com>
2016-05-19 12:28:07 -0400

    [189. Rotate Array][Runtime Error]committed by LeetCode Extension

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
         int[] earray= new int[k];
-        for(int i=nums.length-k;i<nums.length;i++){
-            earray[i+k-nums.length]=nums[i];
+        for(int i=0;i<k;i++){
+            earray[i]=nums[i+nums.length-k];
         }
         for(int i=nums.length-k-1;i>=k;i--)
             nums[i]=nums[i-k];
