d91dcd9e11f2759c83cbff9028f16d0116744ecb
Shiying Li <emma.lishiying@gmail.com>
2016-05-19 15:14:47 -0400

    [169. Majority Element][Accepted]committed by LeetCode Extension

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -1,8 +1,20 @@
 public class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-        //return findMajority(0,nums.length-1,nums);
+//------------Function 1---------------------------------------
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+
+//------------Function 2: vote algorithm-----------------------
+        int main=nums[0];
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                main =nums[i];
+                count++;
+            }else if(main==nums[i]) count++;
+            else count--;
+        }
+       return main; 
     }
     
     // public int findMajority(int start,int end,int[] nums){
