8bc365b4bd24e1efece9bbd5ffad1bc6ad1edb6f
Shiying Li <emma.lishiying@gmail.com>
2016-05-29 19:43:02 -0400

    [119. Pascal's Triangle II][Accepted]committed by LeetCode Extension

---
+++ 119-Pascal's-Triangle-II/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list=new ArrayList<>(rowIndex);
+        for(int i=0;i<=rowIndex;i++){
+            list.add(i,1);
+            for(int j=i-1;j>0;j--){
+                list.set(j,list.get(j-1)+list.get(j));
+            }
+        }
+        return list;
+    }
+}
