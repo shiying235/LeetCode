cb0ed74b9ce4d272c0bfaccc022a7a3fa73bda9e
Shiying Li <emma.lishiying@gmail.com>
2016-06-26 16:52:24 -0400

    [205. Isomorphic Strings][Runtime Error]committed by LeetCode Extension

---
+++ 205-Isomorphic-Strings/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        HashMap sMap = new HashMap();
+        HashMap tMap = new HashMap();
+        for(int i=0;i<sArray.length;i++){
+            if(sMap.containsKey(sArray)) sMap.put(sArray[i],(int)sMap.get(sArray[i])+1);
+            else sMap.put(sArray[i],1);
+            if(tMap.containsKey(tArray)) tMap.put(tArray[i],(int)tMap.get(tArray[i])+1);
+            else tMap.put(tArray[i],1);
+        }
+        if(sMap.size()==tMap.size()){
+            ArrayList sList = (ArrayList)sMap.values();
+            ArrayList tList = (ArrayList)tMap.values();
+            if(sList==tList) return true;
+            else return false;
+            // Set set=tMap.keySet();
+            // while(!map.isEmpty()) {
+            //     if(tMap.containsValue(map.getValue())) tMap.remove()
+                
+            // }
+        }
+        else return false;
+            
+    }
+}
