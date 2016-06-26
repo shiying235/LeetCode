4a6f07a39562c6090a02fa2385b941152f655dc3
Shiying Li <emma.lishiying@gmail.com>
2016-06-26 17:29:25 -0400

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.java
+++ 205-Isomorphic-Strings/solution.java
@@ -1,27 +1,29 @@
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        HashMap sMap = new HashMap();
-        HashMap tMap = new HashMap();
-        for(int i=0;i<sArray.length;i++){
-            if(sMap.containsKey(sArray)) sMap.put(sArray[i],(int)sMap.get(sArray[i])+1);
-            else sMap.put(sArray[i],1);
-            if(tMap.containsKey(tArray)) tMap.put(tArray[i],(int)tMap.get(tArray[i])+1);
-            else tMap.put(tArray[i],1);
+        // char[] sArray = s.toCharArray();
+        // char[] tArray = t.toCharArray();
+        // HashMap sMap = new HashMap();
+        // HashMap tMap = new HashMap();
+        // for(int i=0;i<sArray.length;i++){
+        //     if(sMap.containsKey(sArray)) sMap.put(sArray[i],(int)sMap.get(sArray[i])+1);
+        //     else sMap.put(sArray[i],1);
+        //     if(tMap.containsKey(tArray)) tMap.put(tArray[i],(int)tMap.get(tArray[i])+1);
+        //     else tMap.put(tArray[i],1);
+        // }
+        // if(sMap.size()==tMap.size()){
+        //     List<Integer> sList = new ArrayList<Integer>(sMap.values());
+        //     List<Integer> tList = new ArrayList<Integer>(tMap.values());
+        //     if(sList==tList) return true;
+        //     else return false;
+        // }
+        // else return false;
+        
+        int[] map = new int[512];
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(map[s.charAt(i)]!=map[t.charAt(i)+256]) return false;
+            map[s.charAt(i)]=map[t.charAt(i)+256]=i+1;
         }
-        if(sMap.size()==tMap.size()){
-            List<Integer> sList = (List<Integer>)sMap.values();
-            List<Integer> tList = (List<Integer>)tMap.values();
-            if(sList==tList) return true;
-            else return false;
-            // Set set=tMap.keySet();
-            // while(!map.isEmpty()) {
-            //     if(tMap.containsValue(map.getValue())) tMap.remove()
-                
-            // }
-        }
-        else return false;
-            
+        return true;    
     }
 }
