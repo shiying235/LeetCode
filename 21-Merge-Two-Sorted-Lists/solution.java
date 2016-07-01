f9db2f3bc098ce9456de71439d635832ae493374
Shiying Li <emma.lishiying@gmail.com>
2016-07-01 16:37:41 -0400

    [21. Merge Two Sorted Lists][Runtime Error]committed by LeetCode Extension

---
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode current=l1;
+        ListNode previous=l1;
+        while(current!=null&&l2!=null){
+            if(current.val>=l2.val){
+                ListNode next = current.next;
+                current = new ListNode(l2.val);
+                previous.next=current;
+                current.next=next;
+                previous=current;
+                current=current.next;
+                l2=l2.next;
+            }
+            else{
+                previous=current;
+                current=current.next;
+            }
+        }
+        while(l2!=null){
+            previous=current;
+            current=new ListNode(l1.val);
+            previous.next=current;
+            current=current.next;
+            previous=previous.next;
+            l2=l2.next;
+        }
+        return l1;
+    }
+}
