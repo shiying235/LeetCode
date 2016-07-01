37fedafe4e9a7305b937464bc4b9048fa4d61b24
Shiying Li <emma.lishiying@gmail.com>
2016-07-01 10:51:53 -0400

    [202. Happy Number][Accepted]committed by LeetCode Extension

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public boolean isHappy(int n) {
-        HashMap cycle=new HashMap();
+        HashSet cycle=new HashSet();
         int num=n;
         while(num!=1){
-            if(cycle.containsKey(num)) return false;
-            else cycle.put(num,1);
-            
-            int index=num;
-            int newNum =0;
-            while(index>9){
-                newNum +=  (index%10)*(index%10);
-                index=index/10;
-            }
-           newNum += (index%10)*(index%10);
-            num=newNum;
+            if(cycle.add(num)){
+                 int index=num;
+                int newNum =0;
+                while(index>0){
+                    newNum +=  (index%10)*(index%10);
+                    index=index/10;
+                }
+                num=newNum;
+            }else return false;
+           
         }
         return true;
     }
