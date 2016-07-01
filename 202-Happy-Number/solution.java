b4d42ea240e2b2c20e89bd1fdb463de4a018ef06
Shiying Li <emma.lishiying@gmail.com>
2016-07-01 10:28:22 -0400

    [202. Happy Number][Time Limit Exceeded]committed by LeetCode Extension

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,28 +1,31 @@
 public class Solution {
     public boolean isHappy(int n) {
         HashMap cycle=new HashMap();
-        ArrayList number = new ArrayList<Integer>();
-        int index=n;
-        while(index<10){
-            number.add(0,index%10);
-            index=index/10;
-        }
+        // ArrayList number = new ArrayList<Integer>();
+        // int index=n;
+        // while(index<10){
+        //     number.add(0,index%10);
+        //     index=index/10;
+        // }
         int num=n;
         while(num!=1){
             if(cycle.containsKey(num)) return false;
             else cycle.put(num,1);
             
+            int index=num;
             int newNum =0;
-            for(int i=0;i<number.size();i++){
-                newNum += (int)Math.sqrt((int)number.get(i));
-            }
+            while(index<10){
+               newNum +=  (int)Math.sqrt(index%10);
+               index=index/10;
+           }
+            // for(int i=0;i<number.size();i++){
+            //     newNum += (int)Math.sqrt((int)number.get(i));
+            // }
             num=newNum;
-            number.clear();
-            
-            while(newNum<10){
-                number.add(0,newNum%10);
-                newNum=newNum/10;
-            }
+            // while(newNum<10){
+            //     number.add(0,newNum%10);
+            //     newNum=newNum/10;
+            // }
             
         }
         
