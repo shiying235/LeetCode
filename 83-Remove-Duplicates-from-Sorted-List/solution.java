cc0ffbcb3f1fd276e54055e0da49b1f9a401adde
Shiying Li <emma.lishiying@gmail.com>
2016-07-01 15:47:58 -0400

    [83. Remove Duplicates from Sorted List][Compile Error]committed by LeetCode Extension

---
+++ 83-Remove-Duplicates-from-Sorted-List/solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode ln=head;
+        while(ln!=null){
+            if(ln.val=ln.next.val) ln.next=ln.next.next;
+            else ln=ln.next;
+        }
+        return head;
+    }
+}
