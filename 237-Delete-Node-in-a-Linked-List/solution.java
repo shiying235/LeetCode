0af729c312a4321435849e79467e4952b3660d58
Shiying Li <emma.lishiying@gmail.com>
2016-07-01 14:45:23 -0400

    [237. Delete Node in a Linked List][Compile Error]committed by LeetCode Extension

---
+++ 237-Delete-Node-in-a-Linked-List/solution.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void deleteNode(ListNode node) {
+        while(node.next){
+            node =node.next;
+        }
+        node=null;
+    }
+}
