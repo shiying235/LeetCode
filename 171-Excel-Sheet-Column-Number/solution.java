c28eb6b141f6795af2d82a2536a498071346030f
Shiying Li <emma.lishiying@gmail.com>
2016-07-03 12:36:02 -0400

    [171. Excel Sheet Column Number][Compile Error]committed by LeetCode Extension

---
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int titleToNumber(String s) {
+        // char[] sArray = s.toCharArray();
+        // int sum=0;
+        // for(int i=1;i<sArray.length;i++){
+        //     sum += (sArray[i]-'A'+1)%26*(sArray.length-i-1)*26;
+        // }
+        // sum += (sArray[sArray.length-1]-'A'+1)%26;
+        // return sum;
+        
+        int sum=0;
+        for(int i=0;i<s.size();i++){
+            sum *= 26;
+            sum += (s.charAt(i)-'A')+1;
+        }
+        return sum;
+    }
+}
