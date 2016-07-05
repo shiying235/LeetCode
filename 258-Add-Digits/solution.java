7917f522b4c3eea15b3f646f46f7184b75ad595a
Shiying Li <emma.lishiying@gmail.com>
2016-07-05 09:53:25 -0400

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,11 +1,13 @@
 public class Solution {
     public int addDigits(int num) {
-        if(num<10) return num;
-        int result=0;
-        while(num>0){
-            result += num%10;
-            num =num/10;
-        }
-        return addDigits(result);
+        // if(num<10) return num;
+        // int result=0;
+        // while(num>0){
+        //     result += num%10;
+        //     num =num/10;
+        // }
+        // return addDigits(result);
+        
+        return 1+(num-1)%9;
     }
 }
