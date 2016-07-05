664455b22e9f7065fe7d857661661757066b22cc
Shiying Li <emma.lishiying@gmail.com>
2016-07-05 10:41:40 -0400

    [13. Roman to Integer][Runtime Error]committed by LeetCode Extension

---
+++ 13-Roman-to-Integer/solution.java
@@ -0,0 +1,51 @@
+public class Solution {
+    public int romanToInt(String s) {
+        //：Ⅰ（1）Ⅴ（5）Ⅹ（10）L（50）C（100）D（500）M（1000） 
+        // rules:位于大数的后面时就作为加数；位于大数的前面就作为减数
+        //eg：Ⅲ=3,Ⅳ=4,Ⅵ=6,ⅩⅨ=19,ⅩⅩ=20,ⅩLⅤ=45,MCMⅩⅩC=1980
+        //"DCXXI"
+        HashMap<Character, Integer> table =new HashMap<Character, Integer>();
+        table.put('I',1);
+        table.put('V',5);
+        table.put('X',10);
+        table.put('L',50);
+        table.put('C',100);
+        table.put('D',500);
+        table.put('M',1000);
+        
+        int sum=0;
+        int j=0;
+        int curr=0;
+        for(int i=1;i<s.length()+1;i++){
+            if(i==s.length()+1){
+                sum = sum+ table.get(s.charAt(s.length()-1)) +curr;
+                curr=0;
+            }
+            else{
+            if(table.get(s.charAt(j))>table.get(s.charAt(i))){
+                if(curr!=0){
+                    sum +=curr;
+                    curr=0;
+                }
+                sum += table.get(s.charAt(j));
+                j++;
+            }
+            else if(table.get(s.charAt(j))<table.get(s.charAt(i))){
+               if(curr!=0){
+                    sum -=curr;
+                    curr=0;
+                }
+                sum -= table.get(s.charAt(j));
+                j++;
+            }
+            else{
+                curr += table.get(s.charAt(j));
+                j++;
+            }
+            }
+        }
+        
+            
+        return sum;
+    }
+}
