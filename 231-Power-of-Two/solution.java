b8cb74f1e3843d7bf9955e4fbf062140814e9cf3
Shiying Li <emma.lishiying@gmail.com>
2016-07-05 11:20:31 -0400

    [231. Power of Two][Compile Error]committed by LeetCode Extension

--- 231-Power-of-Two/solution.java
+++ 231-Power-of-Two/solution.java
@@ -1,13 +1,15 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        if(n==1) return true;
+        // if(n<=0) return false;
+        // if(n==1) return true;
         
-        while(n>0){
-            if(n==2) return true;
-            if(n%2!=0) return false;
-            n =n/2;
-        }
-        return true;
+        // while(n>0){
+        //     if(n==2) return true;
+        //     if(n%2!=0) return false;
+        //     n =n/2;
+        // }
+        // return true;
+        
+        return n>0&&(n&(n-1)==0);
     }
 }
