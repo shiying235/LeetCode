b2e3f7cdd8707606e73be2b0220bfd49a70a6e21
Shiying Li <emma.lishiying@gmail.com>
2016-07-06 21:08:54 -0400

    [104. Maximum Depth of Binary Tree][Time Limit Exceeded]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.java
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -10,7 +10,7 @@
 public class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        if(root.left==null&&root.right==null) return 1;
+      //  if(root.left==null&&root.right==null) return 1;
         return maxDepth(root.left)>maxDepth(root.right)?(1+maxDepth(root.left)):(1+maxDepth(root.right));
     }
 }
