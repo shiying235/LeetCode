af918609e825ac3c6ccf08e87c75132d907dcc40
Shiying Li <emma.lishiying@gmail.com>
2016-07-08 14:41:24 -0400

    [344. Reverse String][Accepted]committed by LeetCode Extension

--- 344-Reverse-String/solution.java
+++ 344-Reverse-String/solution.java
@@ -1,24 +1,24 @@
 public class Solution {
     public String reverseString(String s) {
-        // char[] result = s.toCharArray();
-        // int i = 0;
-        // int j = result.length - 1;
-        // while(i<j){
-        //     char temp = result[i];
-        //     result[i] = result [j];
-        //     result[j] = temp;
-        //     i++;
-        //     j--;
-        // }
-        // return new String(result);
-        
-        Stack st = new Stack();
-        for(int i = 0; i < s.length();i++){
-            st.push(s.charAt(i));
+        char[] result = s.toCharArray();
+        int i = 0;
+        int j = result.length - 1;
+        while(i<j){
+            char temp = result[i];
+            result[i] = result [j];
+            result[j] = temp;
+            i++;
+            j--;
         }
-        StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty())
-            sb.append(st.pop());
-        return sb.toString();
+        return new String(result);
+        
+        // Stack st = new Stack();
+        // for(int i = 0; i < s.length();i++){
+        //     st.push(s.charAt(i));
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // while(!st.isEmpty())
+        //     sb.append(st.pop());
+        // return sb.toString();
     }
 }
