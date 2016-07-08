0d2bb14f14a90eb05377a945284b87cf539580c0
Shiying Li <emma.lishiying@gmail.com>
2016-07-08 10:56:10 -0400

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -8,30 +8,42 @@ public class Solution {
         // int lena = a.length()-1;
         // int lenb = b.length()-1;
         // int curr=0;
-        // while(lena>-1&&lenb>-1){
-        //     int sum =aarray[lena]+barray[lenb]+curr;
-        //     int curr=0;
-        //     while(sum>1){
-        //         curr++;
-        //         sum=sum-2;
-        //     }
-        //     sb.
+        // while(lena>-1||lenb>-1||curr==1){
+        //     int ia = lena > -1 ? Character.getNumericValue(aarray[lena--]):0;
+        //     int ib = lenb > -1 ? Character.getNumericValue(barray[lenb--]):0;
+        //     int result =  ia ^ ib ^ curr;
+        //     curr = (ia + ib > 1) ? 1 : 0; 
+        //     sb.append( result);
+            
         // }
 
-        // return null;
+        // return sb.reverse().toString();
         
-        a = new StringBuilder(a).reverse().toString();
-	b = new StringBuilder(b).reverse().toString();
-	int maxLen = Math.max(a.length(), b.length());
-	StringBuilder res = new StringBuilder(maxLen);
-	int ia, ib, carry = 0;
-	for (int i = 0; i <= maxLen; i++) {
-		ia = (i < a.length() ? Integer.parseInt(a.charAt(i) + "") : 0);
-		ib = (i < b.length() ? Integer.parseInt(b.charAt(i) + "") : 0);
-		int sum = ia + ib + carry;
-		carry = sum >= 2 ? 1 : 0;
-		if (i != maxLen || sum != 0) res.append(sum % 2);
-	}
-	return res.reverse().toString();
+        if(a == null || a.isEmpty()) {
+            return b;
+        }
+        if(b == null || b.isEmpty()) {
+            return a;
+        }
+        char[] aArray = a.toCharArray();
+        char[] bArray = b.toCharArray();
+        StringBuilder stb = new StringBuilder();
+
+        int i = aArray.length - 1;
+        int j = bArray.length - 1;
+        int aByte;
+        int bByte;
+        int carry = 0;
+        int result;
+
+        while(i > -1 || j > -1 || carry == 1) {
+            aByte = (i > -1) ? Character.getNumericValue(aArray[i--]) : 0;
+            bByte = (j > -1) ? Character.getNumericValue(bArray[j--]) : 0;
+            result = aByte ^ bByte ^ carry;
+            carry = ((aByte + bByte + carry) >= 2) ? 1 : 0;
+            stb.append(result);
+        }
+        return stb.reverse().toString();
+        
     }
 }
