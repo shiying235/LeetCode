bf51d726f757b9d9eb954d56a66499c3be9ecaea
Shiying Li <emma.lishiying@gmail.com>
2016-07-09 10:00:12 -0400

    [24. Swap Nodes in Pairs][Accepted]committed by LeetCode Extension

--- 24-Swap-Nodes-in-Pairs/solution.java
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -8,17 +8,29 @@
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-        if(head.next==null) return head;
-        ListNode newHead= head.next;
-        ListNode currentNode =head;
-        ListNode swapNode =head.next;
-        while(currentNode!=null&&swapNode!=null){
-            ListNode nextNode =swapNode.next;
-            swapNode.next=currentNode;
-            currentNode.next=nextNode;
-            currentNode=nextNode;
-            swapNode=currentNode.next;
+        if( head==null || head.next == null) return head;
+        ListNode ln = new ListNode(-1);
+        ln.next = head;
+        ListNode current = ln;
+        while( current.next != null && current.next.next != null){
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
         }
-        return newHead;
+        return ln.next;
+        // ListNode newHead= head.next;
+        // ListNode currentNode =head;
+        // ListNode swapNode =head.next;
+        // while(currentNode!=null&&swapNode!=null){
+        //     ListNode nextNode =swapNode.next;
+        //     swapNode.next=currentNode;
+        //     currentNode.next=nextNode;
+        //     currentNode=nextNode;
+        //     swapNode=currentNode.next;
+        // }
+        // return newHead;
     }
 }
