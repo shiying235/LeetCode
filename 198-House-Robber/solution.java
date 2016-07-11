e70deddce37ffddaa6c63e201e0e3f0ba22e6de0
Shiying Li <emma.lishiying@gmail.com>
2016-07-11 13:53:45 -0400

    [198. House Robber][Accepted]committed by LeetCode Extension

---
+++ 198-House-Robber/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int rob(int[] nums) {
+        int a = 0;
+        int b = 0;
+        for( int i = 0; i < nums.length; i++ ){
+            if( i % 2 == 0 )
+                a = Math.max( a+nums[i], b );
+            else
+                b = Math.max( b+nums[i], a );
+        }
+        return Math.max(a,b);
+    }
+}
