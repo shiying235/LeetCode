bef9c2d234d17520ac2145ce699d61a9711e823e
Shiying Li <emma.lishiying@gmail.com>
2016-07-13 16:26:36 -0400

    [162. Find Peak Element][Accepted]committed by LeetCode Extension

---
+++ 162-Find-Peak-Element/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start + 1< end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            else if(nums[mid] > nums[mid + 1] && nums[mid -1] > nums[mid])
+                end = mid;
+            else{
+                start = mid;
+            }
+        }
+        return nums[start] > nums[end] ? start : end;
+    }
+}
